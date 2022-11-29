3b925d67e35788868e25ba5891555dc187555803
az-dev <azzaouihamza.g@gmail.com>
2022-11-29 09:07:39 +0100

    first commit

---
+++ src/main/java/emi/tp/logiciel/tplogiciel/TpLogicielApplication.java
@@ -0,0 +1,13 @@
+package emi.tp.logiciel.tplogiciel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpLogicielApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpLogicielApplication.class, args);
+    }
+
+}
